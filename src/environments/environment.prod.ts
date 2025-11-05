d94813d6e0648c4e8cff9d17c9d347d212cc0512
aemmykun <aemmykun@gmail.com>
2025-11-05 23:31:55 +1100

    Fix build output for Cloudflare Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 ﻿export const environment = {
   production: true,
-  supabaseUrl: 'https://lrblkafbskryjsfmgkcr.supabase.co',
-  supabaseKey: 'sb_publishable_ekUlOet_pHsIAqEGJ5zioQ_Dqn8f_62',
-  backendUrl: 'https://your-deployed-backend-url'
+  supabase: {
+    url: 'https://lrblkafbskryjsfmgkcr.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYmxrYWZic2tyeWpzZm1na2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMjE1OTMsImV4cCI6MjA3NTg5NzU5M30.LoiR7uPNnkXcr-YTKp6cw0j38AOEFHZjEbWDRI_FMU0'
+  },
+  apiUrl: 'https://cloudcleanbed-backend.yourdomain.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 ﻿export const environment = {
   production: false,
-  supabaseUrl: 'https://lrblkafbskryjsfmgkcr.supabase.co',
-  supabaseKey: 'sb_publishable_ekUlOet_pHsIAqEGJ5zioQ_Dqn8f_62',
-  backendUrl: 'http://127.0.0.1:4000'
+  supabase: {
+    url: 'https://lrblkafbskryjsfmgkcr.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyYmxrYWZic2tyeWpzZm1na2NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMjE1OTMsImV4cCI6MjA3NTg5NzU5M30.LoiR7uPNnkXcr-YTKp6cw0j38AOEFHZjEbWDRI_FMU0'
+  },
+  apiUrl: 'http://localhost:4000'
 };
