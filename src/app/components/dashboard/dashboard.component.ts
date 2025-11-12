5a83c0e4874182331877a0b3b721592bdc548067
Arty P <aemmykun@gmail.com>
2025-11-13 07:21:49 +1100

    Update dashboard.component.ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.html',
-  styleUrls: ['./dashboard.css'],
+  selector: 'app/components/dashboard/dashboard.component.ts',
+  templateUrl: './kanban-board.html',
+  styleUrls: ['./kanban-board.css'],
   standalone: true
 })
 export class DashboardComponent {
-  title = 'CloudCleanBed Dashboard';
+  title = 'CloudCleanBed';
 }
