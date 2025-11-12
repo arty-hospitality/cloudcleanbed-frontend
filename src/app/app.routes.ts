6fd1b397beb22bd44304e1a16ec7a33ba98520cf
Arty P <aemmykun@gmail.com>
2025-11-13 07:26:08 +1100

    correct .ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 ﻿import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component.ts';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
